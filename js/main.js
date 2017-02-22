00c680100d474415d028cb676fafa61f2aaa8121
Joel Nilsson <joel.o.nilsson@gmail.com>
2017-02-22 09:04:17 +0100

    Börjat koppla ihop template med objekt i JS

--- js/main.js
+++ js/main.js
@@ -11,6 +11,11 @@ $(()=>{
 	var login = new Login();
 	login.display('body');
 	*/
-	var question = new Question();
+	var question = new Question({
+		id: 10, 
+		imageURL: 'https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcR0pcNSyjJILQeqWiSRLr6U8Qhmvs9YGo_AguL2f8O9snFFSX2OIg',
+		test_id: 1,
+		text: 'What are you going to eat today?'
+	});
 	question.display('body');
 });
