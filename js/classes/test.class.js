63871fb46d5ae1273ee772b8b0709ec250523306
Joel Nilsson <joel.o.nilsson@gmail.com>
2017-02-22 13:20:12 +0100

    Fixat navigeringen mellan frågor

--- js/classes/test.class.js
+++ js/classes/test.class.js
@@ -2,6 +2,7 @@ class Test extends Base{
 	static defaultPropertyValues(){
     return {
 	      id: 0,
+	      currentQuestionIndex: 0,
 	      startingTime: '2017-01-01 09:00', //dateTimeForMySQL(2017-01-01 09:00:00),
 	      endingTime: '2017-01-01 16:00', //dateTimeForMySQL(2017-01-01 16:00:00),
 	      allowedTime: 3,
@@ -16,5 +17,21 @@ class Test extends Base{
 	    if(!(this.questions instanceof QuestionList)){
 	      this.questions = new QuestionList(this.questions);
 	    }
+	}
+
+	get question(){
+		var question = this.questions[this.currentQuestionIndex];
+		return question;
+	}
+
+	nextQuestion(){
+		if(this.questions.length > this.currentQuestionIndex){
+			this.currentQuestionIndex++;
+		}
+	}
+	prevQuestion(){
+		if(this.currentQuestionIndex > 0){
+			this.currentQuestionIndex--;
+		}
 	}
 }
