14980e6bae6562bcb362292a52d58f12f72430c6
Befkadu1 <befkassefa@gmail.com>
2017-03-03 23:43:08 +0100

    createQuestion() method is added to add a new question

--- js/classes/show-test-menu.class.js
+++ js/classes/show-test-menu.class.js
@@ -2,20 +2,18 @@ class ShowTestMenu extends Base {
 
 	constructor(propertyValues){
 		super(propertyValues);
-	}
 
+	}
 	takeTest(index){
-		$('.page-content').empty();
 		// Skriver in indexet på testet man vill visa manuellt än så länge,
 		// sen kan man använda index från när man klickar på test från en lista
+
 		this.getTestFromDB(0,(test)=>{
 			$('.page-content').html('');
-			console.log('test',test);
 			test.display('.page-content');
-			//var testView = new TestView({test:test});
-			//testView.display('.page-content');
 		});
 	}
+
 
 	getTestFromDB(index, callback){
 		this.getTestListFromDB((testList)=>{
@@ -33,19 +31,24 @@ class ShowTestMenu extends Base {
 	showListOfTests(){
 		var listOfTests = new TeacherView();
 		listOfTests.getTests(()=>{
-			listOfTests.display('body');
+			$('.page-content').html('');
+			listOfTests.display('.page-content');
+			//listOfTests.display('body');
 		});
 	}
 
 	showStudentResults(){
 		var result = new StudentResultList();
-        result.load((studentResults)=>{
-        	studentResults.display('body');
-        	/*for(var studentResults of result)
-        	{
-        		studentResults.display('body');
-        	}*/
+        result.load(1, 'java', (studentResults)=>{
+			$('.page-content').html('');
+			result.display('.page-content');	
         	
 		});
+	}
+
+	CreateQuestion(){
+		var addQuestion = new Question();
+		$('.page-content').html('');
+		addQuestion.display('.page-content');
 	}
 }
